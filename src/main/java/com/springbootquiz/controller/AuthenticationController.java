2a28584fd4258e4a4e4891081a486be6a029d6b0
qv92 <hienquoc412018@gmail.com>
2021-10-11 16:22:37 +0700

    fix

--- src/main/java/com/springbootquiz/controller/AuthenticationController.java
+++ src/main/java/com/springbootquiz/controller/AuthenticationController.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
@@ -52,12 +53,13 @@ public class AuthenticationController {
 
     @PostMapping("change-password")
     public ResponseEntity<?> changePass(@RequestBody UserChangePassword user){
-        User user1 = userService.findByUsername(SecurityContextHolder.getContext().getAuthentication().getName()).get();
+        User user1 = userService.findByUsername(SecurityContextHolder.getContext().getAuthentication().getName())
+                .orElseThrow(()->new UsernameNotFoundException(user.getUsername()));
         if (passwordEncoder.matches(user.getOldPassword(),user1.getPassword())){
             user1.setPassword(passwordEncoder.encode(user.getNewPassword()));
             user1.setUpdateTime(LocalDateTime.now());
             userRepository.save(user1);
-            return null;
+            return new ResponseEntity<>("PASSWORD CHANGED , Please sign-in",HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
